c4d1a23fb48de54e52bf94eea42d2082231b9d23
Dan Volchok <77181799+danvolchok@users.noreply.github.com>
2023-12-03 16:08:18 -0500

    created swaggerJSDoc and its options

--- app.js
+++ app.js
@@ -27,12 +27,87 @@ var options = {
     info: {
       title: "Discogs API",
       version: "1.0.0",
+      contact: {
+        name: "Dan Volchok",
+        email: "volchokdan@gmail.com",
+        url: "http://danvolchok.com"
+      }
     },
     servers: [
       {
         url: "http://localhost:3000/api",
       },
     ],
+    tags: {
+      name: "Georgian College"
+    },
+    components: {
+      securitySchemes: {
+        basicAuth: {
+          type: "http",
+          scheme: "basic"
+        }
+      }
+    },
+    paths: {
+      '/tracks': {
+        get: {
+          tags: ['Georgian College'],
+          security: [
+            {
+              basicAuth: []
+            }
+          ],
+          description: 'Returns a list of tracks',
+          parameters: [
+            {
+              name: 'year',
+              in: 'query',
+              required: true,
+              description: 'Release year of track',
+              schema: {
+                type: 'string'
+              }
+            },
+            {
+              name: 'genre',
+              in: 'query',
+              required: true,
+              description: 'Track genre',
+              schema: {
+                type: 'string'
+              }
+            }
+          ],
+          responses: {
+            '200': {
+              description: 'Unfiltered list of tracks',
+              content: {
+                'application/json': {
+                  schema: {
+                    type: 'array',
+                    items: {
+                      type: 'object',
+                      properties: {
+                        title: {
+                          type: 'string',
+                          description: 'Name of the track'
+                        },
+                        artist: {
+                          type: 'string',
+                          description: 'Artist of the track'
+                        },
+                        // Add other track properties as needed
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
   },
   apis: ["./routes/api/*.js"],
 };
@@ -40,12 +115,14 @@ var swaggerSpec = swaggerJSDoc(options);
 
 var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
+var tracksRouter = require('./routes/api/tracks');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
---
+++ routes/api/tracks.js
@@ -0,0 +1,117 @@
+// Express package
+const express = require("express");
+// router object
+const router = express.Router();
+// import model
+const Track = require("../../models/track");
+
+// pagination
+const pageSize = 10;
+
+// POST
+router.post("/", async (req, res, next) => {
+  if (!req.body.title) {
+    res.status(400).json({ validationError: "Title is a required field." });
+  } else if (!req.body.artist) {
+    res.status(400).json({ validationError: "Artist is a required field." });
+  } else if (!req.body.album) {
+    res.status(400).json({ validationError: "Album is a required field." });
+  } else if (!req.body.year) {
+    res.status(400).json({ validationError: "Year is a required field." });
+  } else if (!req.body.genre) {
+    res.status(400).json({ validationError: "Genre is a required field." });
+  } else {
+    let track = new Track({
+      title: req.body.title,
+      artist: req.body.artist,
+      album: req.body.album,
+      year: req.body.year,
+      genre: req.body.genre
+    });
+    await track.save();
+    res.status(201).json(track);
+  }
+});
+
+// GET
+/**
+ * @openapi
+ * /projects:
+ *  get:
+ *    description: List all projects
+ *    responses:
+ *      200:
+ *        description: Returns a list of projects
+ */
+
+router.get("/", async (req, res, next) => {
+    // pagination
+  let page = req.query.page || 1; // if page is null, default to 1
+
+  //calculate records to skip
+  let skipSize = pageSize * (page - 1);
+
+  // empty object to represent query
+  // retrieve query string for filtering
+
+  // tracks?year=X
+  let query = {};
+  if (req.query.year) {
+    query.year = req.query.year;
+  }
+
+  // tracks?genre=Y
+  if (req.query.genre) {
+    query.genre = req.query.genre;
+  }
+
+  // tracks?artist=Z
+  if (req.query.artist) {
+    query.artist = req.query.artist
+  }
+
+  let tracks = await Track.find(query)
+    .sort([["year", "descending"]])
+    .skip(skipSize)
+    .limit(pageSize);
+  res.status(200).json(tracks);
+})
+
+// PUT
+
+router.put("/:_id", async (req, res, next) => {
+    if (!req.body.title) {
+        res.status(400).json({ validationError: "Title is a required field." });
+      } else if (!req.body.artist) {
+        res.status(400).json({ validationError: "Artist is a required field." });
+      } else if (!req.body.album) {
+        res.status(400).json({ validationError: "Album is a required field." });
+      } else if (!req.body.year) {
+        res.status(400).json({ validationError: "Year is a required field." });
+      } else if (!req.body.genre) {
+        res.status(400).json({ validationError: "Genre is a required field." });
+    } else {
+      let track = await Track.findByIdAndUpdate(
+        req.params._id,
+        {
+            title: req.body.title,
+            artist: req.body.artist,
+            album: req.body.album,
+            year: req.body.year,
+            genre: req.body.genre
+        },
+        { new: true } // mongoose returns the updated version of this
+      );
+      res.status(200).json(track);
+    }
+  });
+
+  // DELETE
+
+  router.delete("/:_id", async (req, res, next) => {
+    await Track.findByIdAndDelete(req.params._id);
+    res.status(200).json({ success: "true" });
+  });
+  
+  // Export
+  module.exports = router;
